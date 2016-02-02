5dc5f397f341c93e955a5d1cc9c8b2d6984507c6
Hans Sowa <hanssowa@gmail.com>
2016-02-02 18:22:02 +0100

    Sourcemap are loaded with karma-sourcemap-loader.

--- karma.conf.js
+++ karma.conf.js
@@ -1,5 +1,9 @@
 module.exports = function(config) {
     config.set({
+        plugins: [
+            'karma-systemjs', 'karma-sourcemap-loader', 'karma-jasmine', 'karma-phantomjs-launcher'
+        ],
+
         frameworks: ['systemjs', 'jasmine'],
 
         systemjs: {
@@ -7,8 +11,16 @@ module.exports = function(config) {
         },
 
         files: [
-            'build/**/*.js'
+            'build/**/*.js',
+            {pattern: "build/**/*.js.map", served: true, included: false, watched: false},
+            {pattern: "src/**/*.ts", served: true, included: false, watched: false}
         ],
+
+        preprocessors: {
+            'build/**/*.js': ['sourcemap']
+        },
+
+        logLevel: 'config.LOG_DEBUG',
 
         browsers: ['PhantomJS']
 
